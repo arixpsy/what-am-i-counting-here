e273241c29c9a5a5b9d5e3afb3393d8015ac22fc
Phua Si Yu Arix <42609059+arixpsy@users.noreply.github.com>
2023-02-28 02:28:44 +0800

    wip: add login button to index page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import Button from '@/components/commons/Button/Button.svelte';
+</script>
 
-<h1 class="text-3xl font-bold underline">
-  Hello world!
-</h1>
+<div class="container mx-auto min-h-screen pt-9">
+	<h1 class="sticky top-0 z-30 bg-white py-3  text-center text-4xl">WAICH</h1>
+
+  <div class='flex justify-center pt-6'>
+    <a href='/api/login/github'>
+      <Button>Login to Github</Button>
+    </a>
+  </div>
+</div>
