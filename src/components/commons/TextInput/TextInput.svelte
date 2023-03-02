594ce7bc688ac7f4b9983a20ddeb12416963fcbf
Phua Si Yu Arix <42609059+arixpsy@users.noreply.github.com>
2023-03-03 00:28:54 +0800

    feat: create <TextInput />

---
+++ src/components/commons/TextInput/TextInput.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	export let value: string
+	export let ref: HTMLInputElement | undefined = undefined
+	export let hasError: boolean = false
+</script>
+
+<input
+	type="text"
+	bind:value
+	bind:this={ref}
+	class="block w-full rounded-lg border-2 border-gray-50 bg-gray-50 p-2.5 text-sm text-gray-900 outline-none transition-colors focus:border-blue-700"
+	class:has-error={hasError}
+/>
+
+<style>
+	.has-error {
+		@apply border-red-400;
+	}
+</style>
--- src/components/commons/index.ts
+++ src/components/commons/index.ts
@@ -1,3 +1,4 @@
 import Button from '@/components/commons/Button/Button.svelte';
+import TextInput from '@/components/commons/TextInput/TextInput.svelte';
 
-export { Button };
+export { Button, TextInput };
