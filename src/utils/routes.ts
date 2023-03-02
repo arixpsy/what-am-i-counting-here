47c26d7ed03c0d3cc130b161aa9237f56afbfe01
Phua Si Yu Arix <42609059+arixpsy@users.noreply.github.com>
2023-03-03 00:02:04 +0800

    wip: add more Route const

--- src/utils/routes.ts
+++ src/utils/routes.ts
@@ -1,10 +1,16 @@
 export const Routes = {
-  // API
-  GITHUB_AUTHORIZATION: '/api/login/github',
+	// API
+	GITHUB_AUTHORIZATION: '/api/login/github',
 
-  // PAGES
-  LOGIN: '/',
-  HOME: '/home',
-  COUNTER: '/counter',
-  HISTORY: '/history'
+	// PAGES
+	LOGIN: '/',
+	HOME: '/home',
+	COUNTER: '/counter',
+	HISTORY: '/history',
 } as const
+
+export type Routes = (typeof Routes)[keyof typeof Routes]
+
+export const AUTH_PAGE_ROUTES = [Routes.HOME, Routes.COUNTER, Routes.HISTORY] as const
+
+export const PAGE_ROUTES = [Routes.LOGIN, ...AUTH_PAGE_ROUTES] as const
