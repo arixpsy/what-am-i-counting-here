a0e917704909707c1fac1020ffaf0cb50025fc3c
Phua Si Yu Arix <42609059+arixpsy@users.noreply.github.com>
2023-03-03 00:59:40 +0800

    tweak: swap crypto lib

--- src/routes/api/login/github/+server.ts
+++ src/routes/api/login/github/+server.ts
@@ -1,7 +1,7 @@
 import { GITHUB_OAUTH_CLIENT_ID } from '$env/static/private'
 import { CookieKey } from '@/@types/commons/cookies'
 import { redirect } from '@sveltejs/kit'
-import { randomBytes } from 'crypto'
+import randomBytes from 'random-bytes'
 import type { RequestHandler } from './$types'
 
 const GITHUB_AUTHORIZE_URL = 'https://github.com/login/oauth/authorize'
@@ -9,8 +9,8 @@ const GITHUB_AUTHORIZE_URL = 'https://github.com/login/oauth/authorize'
 // [GET]: api/login/github
 // Generates a oauth state parameter and stores it as a cookie for 10 minutes.
 // Redirect users to github's oauth authorize endpoint with the client id and state in URLSearchParams.
-export const GET: RequestHandler = ({ cookies }) => {
-	const oauthState = randomBytes(16).toString('base64')
+export const GET: RequestHandler = async ({ cookies }) => {
+	const oauthState = randomBytes.sync(16).toString('base64')
 
 	cookies.set(CookieKey.GITHUB_OAUTH_STATE, oauthState, {
 		httpOnly: true,
