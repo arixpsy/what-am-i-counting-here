f89d8cdb32240af7161f1e9624d83c93d776a508
Phua Si Yu Arix <42609059+arixpsy@users.noreply.github.com>
2023-03-02 22:34:48 +0800

    wip: add waich token cookie key and token lifespan

--- src/@types/commons/cookies.ts
+++ src/@types/commons/cookies.ts
@@ -1,3 +1,7 @@
 export const CookieKey = {
-  GITHUB_OAUTH_STATE: 'oauth_state'
+  GITHUB_OAUTH_STATE: 'oauth_state',
+  WAICH_TOKEN: 'waich-token'
 } as const
+
+// 30 days in seconds
+export const WAICH_TOKEN_LIFESPAN = 2.592e+6
