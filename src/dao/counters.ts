4faf113a588c0b6c0446a5917cee806450e7087a
Phua Si Yu Arix <42609059+arixpsy@users.noreply.github.com>
2023-03-04 03:52:29 +0800

    wip: create CountersDao

---
+++ src/dao/counters.ts
@@ -0,0 +1,30 @@
+import { PrismaClient } from '@prisma/client'
+import type { NewCounter } from '@/@types/api/counters'
+const prisma = new PrismaClient()
+
+const createCounter = (data: NewCounter) =>
+	prisma.counter.create({
+		data,
+	})
+
+const findAllByUserId = (userId: number) =>
+	prisma.counter.findMany({
+		where: {
+			userId,
+		},
+	})
+
+const findById = (counterId: number) =>
+	prisma.counter.findUnique({
+		where: {
+			id: counterId,
+		},
+	})
+
+const CountersDao = {
+	createCounter,
+	findAllByUserId,
+	findById,
+}
+
+export default CountersDao
