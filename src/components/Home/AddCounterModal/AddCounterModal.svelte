e926577f3cde99d1954a78873011570eaad30c9c
Phua Si Yu Arix <42609059+arixpsy@users.noreply.github.com>
2023-03-04 02:10:09 +0800

    feat: create <AddCounterModal />

---
+++ src/components/Home/AddCounterModal/AddCounterModal.svelte
@@ -0,0 +1,159 @@
+<script lang="ts">
+	import { createEventDispatcher, tick } from 'svelte'
+	import { fade } from 'svelte/transition'
+	import type { z } from 'zod'
+	import {
+		Modal,
+		TextInput,
+		RadioInput,
+		NumberInput,
+		SwitchInput,
+		ColorRadioInput,
+		IncrementInput,
+		FormItem,
+	} from '@/components/commons'
+	import {
+		CounterColors,
+		type CounterColor,
+		CounterFormSchema,
+		type NewCounterRequest,
+	} from '@/@types/api/counters'
+	import { ResetType } from '@prisma/client'
+	import { useForm } from '@/hooks/useForm'
+
+	export let isVisible: boolean = false
+
+	const {
+		form,
+		refs,
+		errors,
+		onSubmit: handleFormSubmit,
+		resetForm: handleResetForm,
+	} = useForm({
+		schema: CounterFormSchema,
+		defaultValues: {
+			title: '',
+			resetType: ResetType.Day,
+			hasTarget: false,
+			target: 0,
+			color: CounterColors[0],
+			hasCustomIncrement: false,
+			increment: 1,
+		},
+		onSubmitCallback(values: z.infer<typeof CounterFormSchema>) {
+			const { title, resetType, increment, target, color } = values
+			const newCounter: NewCounterRequest = {
+				title,
+				resetType,
+				increment,
+				target,
+				color,
+			}
+			console.log(newCounter)
+			// TODO: Call API
+			dispatch('modal-close')
+			handleResetForm()
+		},
+	})
+	const dispatch = createEventDispatcher()
+
+	$: resetTypeValues = Object.entries(ResetType)
+	$: ({ hasTarget } = form)
+	$: if (isVisible) {
+		focusFirstField()
+	}
+	$: if (hasTarget) {
+		focusTargetRef()
+	}
+
+	async function focusFirstField() {
+		await tick()
+		refs.title?.focus()
+	}
+
+	async function focusTargetRef() {
+		await tick()
+		refs.target?.focus()
+	}
+
+	function handleFormCancel() {
+		dispatch('modal-cancel')
+		handleResetForm()
+	}
+
+	function handleFormClose() {
+		dispatch('modal-close')
+		handleResetForm()
+	}
+
+	function handleRadioSelect(event: CustomEvent<ResetType>) {
+		form.resetType = event.detail
+	}
+
+	function handleColorRadioSelect(event: CustomEvent<CounterColor>) {
+		form.color = event.detail
+	}
+</script>
+
+<Modal
+	{isVisible}
+	on:modal-confirm={handleFormSubmit}
+	on:modal-cancel={handleFormCancel}
+	on:modal-close={handleFormClose}
+>
+	<p class="mb-3 text-3xl">Create</p>
+	<form on:submit|preventDefault={handleFormSubmit} class="flex flex-col">
+		<FormItem label="What are you counting?" errorMessage={$errors.title}>
+			<TextInput bind:value={form.title} bind:ref={refs.title} hasError={!!$errors.title} />
+		</FormItem>
+		<FormItem label="Resets every:">
+			<div class="flex flex-wrap gap-3">
+				{#each resetTypeValues as [resetKey, resetType] (resetKey)}
+					<RadioInput
+						name="reset_type"
+						bind:group={form.resetType}
+						value={resetType}
+						on:radio-select={handleRadioSelect}
+						on:radio-submit={handleFormSubmit}
+					/>
+				{/each}
+			</div>
+		</FormItem>
+		<FormItem label="Counter increment:">
+			<IncrementInput
+				bind:value={form.increment}
+				bind:isCustom={form.hasCustomIncrement}
+				on:switch-submit={handleFormSubmit}
+			/>
+		</FormItem>
+		<FormItem label="Set a target?" errorMessage={$errors.target}>
+			<div class="flex flex-wrap items-center gap-3">
+				<SwitchInput bind:isToggle={form.hasTarget} on:switch-submit={handleFormSubmit} />
+				{#if form.hasTarget}
+					<div class="flex flex-1 flex-wrap items-center gap-3" transition:fade>
+						<p class="flex-none text-sm text-gray-500">Target:</p>
+						<NumberInput
+							bind:ref={refs.target}
+							bind:value={form.target}
+							class="flex-1"
+							hasError={!!$errors.target}
+						/>
+					</div>
+				{/if}
+			</div>
+		</FormItem>
+		<FormItem label="Pick a color:">
+			<div class="flex max-w-xl flex-wrap gap-3">
+				{#each CounterColors as color, i (i)}
+					<ColorRadioInput
+						bind:group={form.color}
+						value={color}
+						on:radio-select={handleColorRadioSelect}
+						on:radio-submit={handleFormSubmit}
+					/>
+				{/each}
+			</div>
+		</FormItem>
+		<input type="submit" hidden />
+	</form>
+</Modal>
---
+++ src/components/Home/index.ts
@@ -0,0 +1,3 @@
+import AddCounterModal from '@/components/Home/AddCounterModal/AddCounterModal.svelte'
+
+export { AddCounterModal }
