10b216ebcff29c0526c3ede39f4b182acee0ad9e
Phua Si Yu Arix <42609059+arixpsy@users.noreply.github.com>
2023-03-03 23:41:53 +0800

    feat: create <NumberInput />

---
+++ src/components/commons/NumberInput/NumberInput.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	export let value: number
+	export let ref: HTMLInputElement | undefined = undefined
+	export let hasError: boolean = false
+	export { forwardClasses as class }
+
+	let forwardClasses: string = ''
+</script>
+
+<input
+	type="number"
+	bind:value
+	bind:this={ref}
+	class={`${forwardClasses} block w-full rounded-lg border-2 border-gray-50 bg-gray-50 p-2.5 text-sm text-gray-900 outline-none transition-colors focus:border-blue-700`}
+	class:has-error={hasError}
+	step="0.01"
+/>
+
+<style>
+	.has-error {
+		@apply border-red-400;
+	}
+</style>
--- src/components/commons/index.ts
+++ src/components/commons/index.ts
@@ -1,6 +1,7 @@
-import Button from '@/components/commons/Button/Button.svelte';
-import Modal from '@/components/commons/Modal/Modal.svelte';
-import RadioInput from '@/components/commons/RadioInput/RadioInput.svelte';
-import TextInput from '@/components/commons/TextInput/TextInput.svelte';
+import Button from '@/components/commons/Button/Button.svelte'
+import Modal from '@/components/commons/Modal/Modal.svelte'
+import NumberInput from '@/components/commons/NumberInput/NumberInput.svelte'
+import RadioInput from '@/components/commons/RadioInput/RadioInput.svelte'
+import TextInput from '@/components/commons/TextInput/TextInput.svelte'
 
-export { Button, Modal, RadioInput, TextInput };
+export { Button, Modal, NumberInput, RadioInput, TextInput }
