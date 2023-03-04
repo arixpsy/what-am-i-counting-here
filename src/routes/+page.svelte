5673a6ee7b4e71d01eb30add7be81ef1cc3803e9
Phua Si Yu Arix <42609059+arixpsy@users.noreply.github.com>
2023-03-05 02:56:36 +0800

    lint: prettier

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,18 @@
-<script lang='ts'>
-	import Button from '@/components/commons/Button/Button.svelte';
+<script lang="ts">
+	import Button from '@/components/commons/Button/Button.svelte'
 	import { Routes } from '@/utils/routes'
 
-  const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone
-  const loginSearchParams = new URLSearchParams()
-  loginSearchParams.append('tz', timezone)
-
+	const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone
+	const loginSearchParams = new URLSearchParams()
+	loginSearchParams.append('tz', timezone)
 </script>
 
 <div class="container mx-auto min-h-screen pt-9">
 	<h1 class="sticky top-0 z-30 bg-white py-3  text-center text-4xl">WAICH</h1>
 
-  <div class='flex justify-center pt-6'>
-    <a href={`${Routes.GITHUB_AUTHORIZATION}?${loginSearchParams.toString()}`}>
-      <Button>Login to Github</Button>
-    </a>
-  </div>
+	<div class="flex justify-center pt-6">
+		<a href={`${Routes.GITHUB_AUTHORIZATION}?${loginSearchParams.toString()}`}>
+			<Button>Login to Github</Button>
+		</a>
+	</div>
 </div>
