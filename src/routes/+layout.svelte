d26ae1150bfc1711f775bfe440ef4a20e2d3c578
Phua Si Yu Arix <42609059+arixpsy@users.noreply.github.com>
2023-03-05 01:33:44 +0800

    wip: configure svelte query for ssr

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,12 @@
 <script>
+	import { browser } from '$app/environment'
 	import { QueryClient, QueryClientProvider } from '@sveltestack/svelte-query'
 	import '../app.css'
 
 	const client = new QueryClient({
 		defaultOptions: {
 			queries:  {
+				enabled: browser,
 				refetchOnWindowFocus: false
 			}
 		}
