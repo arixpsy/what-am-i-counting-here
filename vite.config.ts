0d769c5bf196f8ffa4717802217c13cd3ff25ac6
Phua Si Yu Arix <42609059+arixpsy@users.noreply.github.com>
2023-03-05 07:12:54 +0800

    tweak: test prisma client issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,16 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vitest/config';
-import { resolve } from 'path';
+import { sveltekit } from '@sveltejs/kit/vite'
+import { defineConfig } from 'vitest/config'
+import { resolve } from 'path'
 
 export default defineConfig({
 	plugins: [sveltekit()],
 	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
+		include: ['src/**/*.{test,spec}.{js,ts}'],
 	},
 	resolve: {
 		alias: {
-			'@': resolve('./src')
-		}
-	}
-});
+			'@': resolve('./src'),
+			'.prisma/client/index-browser': './node_modules/.prisma/client/index-browser.js',
+		},
+	},
+})
