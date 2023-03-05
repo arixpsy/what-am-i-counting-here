e423a2d54dafaa2a908206486713f07a9ee08606
Phua Si Yu Arix <42609059+arixpsy@users.noreply.github.com>
2023-03-06 01:19:21 +0800

    wip: add capitalize helper function

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -1,5 +1,11 @@
 export const formatCount = (count: number) =>
-	(count).toLocaleString(undefined, {
+	count.toLocaleString(undefined, {
 		maximumFractionDigits: 2,
 		minimumFractionDigits: 0,
 	})
+
+export const capitalizeWord = (text: string) =>
+	text
+		.split(' ')
+		.map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+		.join(' ')
