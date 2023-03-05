9e6836ba72c916498bfb897ad9319dba8bb2f0cb
Phua Si Yu Arix <42609059+arixpsy@users.noreply.github.com>
2023-03-05 19:59:10 +0800

    feat: create <IncrementPreviewTile />

---
+++ src/components/Home/IncrementPreviewTile/IncrementPreviewTile.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { Icon } from '@/components/commons'
+	import { formatCount } from '@/utils/format'
+	import type { GetCounterResponse } from '@/@types/api/counters'
+
+	export let counter: GetCounterResponse | undefined = undefined
+	export let increment: number
+</script>
+
+<div
+	class="flex aspect-square w-full max-w-xs flex-col items-center justify-center self-center rounded-lg p-2"
+	style="background-color: {counter?.color}"
+>
+	<p class="w-full truncate text-center text-3xl text-gray-100">
+		{counter?.title}
+	</p>
+	<p class="my-3 flex flex-wrap items-center justify-center space-x-3 text-3xl font-bold">
+		<span class="line-through">{formatCount(counter?.currentCount || 0)}</span>
+		<Icon.ChevronRight />
+		<span>{formatCount(counter?.currentCount || 0 + increment)}</span>
+	</p>
+</div>
--- src/components/Home/index.ts
+++ src/components/Home/index.ts
@@ -1,4 +1,5 @@
 import AddCounterModal from '@/components/Home/AddCounterModal/AddCounterModal.svelte'
 import CounterTile from '@/components/Home/CounterTile/CounterTile.svelte'
+import IncrementPreviewTile from '@/components/Home/IncrementPreviewTile/IncrementPreviewTile.svelte'
 
-export { AddCounterModal, CounterTile }
+export { AddCounterModal, CounterTile, IncrementPreviewTile }
