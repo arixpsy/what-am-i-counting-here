04f9ab18422df440fee9aa5b8801ee59520bb096
Phua Si Yu Arix <42609059+arixpsy@users.noreply.github.com>
2023-03-07 01:51:43 +0800

    wip: removed unused dao methods

--- src/dao/labels.ts
+++ src/dao/labels.ts
@@ -1,11 +1,5 @@
-import type { NewLabel } from '@/@types/api/labels'
 import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
-
-const createLabel = (data: NewLabel) =>
-	prisma.label.create({
-		data,
-	})
 
 const findAllByUserId = (userId: number, searchPhrase: string) =>
 	prisma.label.findMany({
@@ -24,18 +18,8 @@ const findAllByUserId = (userId: number, searchPhrase: string) =>
 		take: 5,
 	})
 
-const findByLabel = (userId: number, value: string) =>
-	prisma.label.findFirst({
-		where: {
-			userId,
-			value,
-		},
-	})
-
 const LabelsDao = {
-	createLabel,
 	findAllByUserId,
-	findByLabel,
 }
 
 export default LabelsDao
