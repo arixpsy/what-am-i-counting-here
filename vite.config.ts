be3fad5117b96a67e4d1afca9dd21b1089ceacb4
Phua Si Yu Arix <42609059+arixpsy@users.noreply.github.com>
2023-03-08 22:09:11 +0800

    tweak: changing pwa icon purpose

--- vite.config.ts
+++ vite.config.ts
@@ -22,6 +22,12 @@ export default defineConfig({
 						type: 'image/png',
 					},
 					{
+						src: '/waich-192x192.png',
+						sizes: '192x192',
+						type: 'image/png',
+						purpose: 'maskable',
+					},
+					{
 						src: '/waich-512x512.png',
 						sizes: '512x512',
 						type: 'image/png',
@@ -30,7 +36,7 @@ export default defineConfig({
 						src: '/waich-512x512.png',
 						sizes: '512x512',
 						type: 'image/png',
-						purpose: 'any maskable',
+						purpose: 'maskable',
 					},
 				],
 			},
