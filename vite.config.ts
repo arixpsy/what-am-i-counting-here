cd73bc9700bb512b4a0d976626721b4ff2cb8e6d
Phua Si Yu Arix <42609059+arixpsy@users.noreply.github.com>
2023-03-08 19:15:54 +0800

    cleanup: remove dev pwa injection

--- vite.config.ts
+++ vite.config.ts
@@ -4,40 +4,38 @@ import { resolve } from 'path'
 import { SvelteKitPWA } from '@vite-pwa/sveltekit'
 
 export default defineConfig({
-	plugins: [sveltekit(), SvelteKitPWA({
-		manifest: {
-			short_name: 'WAICH',
-			name: 'What am I counting here',
-			start_url: '/',
-			scope: '/',
-			display: 'standalone',
-			theme_color: "#ffffff",
-			background_color: "#ffffff",
-			icons: [
-				{
-					src: '/waich-192x192.png',
-					sizes: '192x192',
-					type: 'image/png',
-				},
-				{
-					src: '/waich-512x512.png',
-					sizes: '512x512',
-					type: 'image/png',
-				},
-				{
-					src: '/waich-512x512.png',
-					sizes: '512x512',
-					type: 'image/png',
-					purpose: 'any maskable',
-				},
-			],
-		},
-		// devOptions: {
-		// 	enabled: true,
-		// 	type: 'module',
-		// 	navigateFallback: '/',
-		// },
-	})],
+	plugins: [
+		sveltekit(),
+		SvelteKitPWA({
+			manifest: {
+				short_name: 'WAICH',
+				name: 'What am I counting here',
+				start_url: '/',
+				scope: '/',
+				display: 'standalone',
+				theme_color: '#ffffff',
+				background_color: '#ffffff',
+				icons: [
+					{
+						src: '/waich-192x192.png',
+						sizes: '192x192',
+						type: 'image/png',
+					},
+					{
+						src: '/waich-512x512.png',
+						sizes: '512x512',
+						type: 'image/png',
+					},
+					{
+						src: '/waich-512x512.png',
+						sizes: '512x512',
+						type: 'image/png',
+						purpose: 'any maskable',
+					},
+				],
+			},
+		}),
+	],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}'],
 	},
