89fd12e69b485d80cbb6d483e637c0edcd3558d0
Phua Si Yu Arix <42609059+arixpsy@users.noreply.github.com>
2023-03-09 12:57:56 +0800

    wip: add interfaces for Records

--- src/@types/api/records.ts
+++ src/@types/api/records.ts
@@ -1,3 +1,22 @@
-import type { Record } from '@prisma/client'
+import type { Record, Counter, Label } from '@prisma/client'
 
 export type NewRecord = Pick<Record, 'counterId' | 'description' | 'increment' | 'userId'>
+
+export type RecordWithCounterAndLabel = Record & {
+	counter: Counter
+	labels: Array<Label>
+}
+
+export type GetRecordHistoryRequest = {
+	counterId?: number
+	size?: number
+	cursor?: number
+}
+
+export type GetRecordHistoryResponse = {
+	cursorNext?: number
+	data: Array<{
+		date: Date
+		records: Array<RecordWithCounterAndLabel>
+	}>
+}
