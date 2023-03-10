1488beee90615c19e63a391ba7c8566ea43bc778
Phua Si Yu Arix <42609059+arixpsy@users.noreply.github.com>
2023-03-10 23:11:09 +0800

    feat: finish history page

--- src/routes/history/+page.svelte
+++ src/routes/history/+page.svelte
@@ -1,10 +1,30 @@
 <script lang="ts">
-	import { goto } from '$app/navigation'
-	import { Icon, NavigationItem } from '@/components/commons'
-	import { Routes } from '@/utils/routes'
+	import { flip } from 'svelte/animate'
 	import { fade } from 'svelte/transition'
+	import { useInfiniteQuery } from '@sveltestack/svelte-query'
+	import { DateTime } from 'luxon'
+	import { goto } from '$app/navigation'
+	import { Icon, NavigationItem, Loader } from '@/components/commons'
+	import { HistoryRecord } from '@/components/History'
+	import { visible } from '@/actions/visible'
+	import { callGetRecordHistory } from '@/utils/fetch/records'
+	import { Routes } from '@/utils/routes'
+	import { QueryKey } from '@/utils/fetch/queryKeys'
 
 	let scrollY: number
+	let size: number = 10
+
+	const historyRecords = useInfiniteQuery(
+		QueryKey.GET_RECORD_HISTORY,
+		({ pageParam = undefined }) =>
+			callGetRecordHistory({
+				size,
+				cursor: pageParam,
+			}),
+		{
+			getNextPageParam: (lastPage) => lastPage.cursorNext,
+		}
+	)
 </script>
 
 <svelte:window bind:scrollY />
@@ -19,7 +39,32 @@
 	</h1>
 
 	<div class="mx-auto mb-24 flex w-full max-w-lg flex-col py-3">
-		<div>Records</div>
+		{#if $historyRecords.data?.pages}
+			{#each $historyRecords.data.pages as page}
+				{#each page.data as { date, records } (date)}
+					<h2 class="col-span-2 my-3 px-3 text-lg text-gray-400 first:mt-0" transition:fade>
+						{DateTime.fromJSDate(new Date(date)).toFormat('ccc, d LLL y')}
+					</h2>
+					{#each records as record, index (record.id)}
+						<div animate:flip={{ duration: 400 }} out:fade>
+							<HistoryRecord {record} {index} />
+						</div>
+					{/each}
+				{/each}
+			{/each}
+		{/if}
+
+		{#if !$historyRecords.isFetching && $historyRecords.hasNextPage}
+			<div class="flex justify-center">
+				<div use:visible on:visible={$historyRecords.fetchNextPage}>
+					<Loader />
+				</div>
+			</div>
+		{/if}
+
+		{#if $historyRecords.isFetched && !$historyRecords.hasNextPage}
+			<div class="mt-3 text-center text-gray-400">End of history</div>
+		{/if}
 	</div>
 
 	<!-- NAVIGATION TO HOME-->
